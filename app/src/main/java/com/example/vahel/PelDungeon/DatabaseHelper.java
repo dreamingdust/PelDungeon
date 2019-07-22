643096991686f97ea18469222fa31cb323311ba9
Vahel <Vahel.Hassan123@gmail.com>
2019-07-22 13:52:13 +0200

    Database: Create insert example for table Weapons Melee. Delete dublicate for all tables.

--- app/src/main/java/com/example/vahel/PelDungeon/DatabaseHelper.java
+++ app/src/main/java/com/example/vahel/PelDungeon/DatabaseHelper.java
@@ -7,6 +7,7 @@
 
 package com.example.vahel.PelDungeon;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -23,7 +24,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public final static String COL_5 = "DELAY";
 
     /* Tabelle für Weapons erzeugen: Weapons Melee */
-    public final static String TABLE_MISSILE  = "weapons_missile";
+    public final static String TABLE_MISSILE = "weapons_missile";
     public final static String COL_6 = "ID";
     public final static String COL_7 = "NAME";
     public final static String COL_8 = "REQUIRED_STRENGTH";
@@ -32,12 +33,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public final static String COL_11 = "DELAY";
 
     /* Tabelle für Weapons erzeugen: Weapons Enchantments */
-    public final static String TABLE_ENCHANTMENTS  = "weapons_enchantments";
+    public final static String TABLE_ENCHANTMENTS = "weapons_enchantments";
     public final static String COL_12 = "ID";
     public final static String COL_13 = "NAME";
     /*-----------------------------------------------ARMORS-----------------------------------------------*/
     /* Tabelle für Armors erzeugen*/
-    public final static String TABLE_ARMORS  = "all_armors";
+    public final static String TABLE_ARMORS = "all_armors";
     public final static String COL_14 = "ID";
     public final static String COL_15 = "NAME";
     public final static String COL_16 = "LEVEL";
@@ -46,7 +47,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public final static String COL_19 = "EFFECTIVE_ABSORPTION";
     /*-----------------------------------------------RINGS-----------------------------------------------*/
     /* Tabelle für Rings erzeugen*/
-    public final static String TABLE_RINGS  = "all_rings";
+    public final static String TABLE_RINGS = "all_rings";
     public final static String COL_20 = "ID";
     public final static String COL_21 = "NAME";
     public final static String COL_22 = "LEVEL";
@@ -56,7 +57,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public final static String COL_26 = "RESISTENCE_PROBABILITY";
     /*-----------------------------------------------WANDS-----------------------------------------------*/
     /* Tabelle für Wands erzeugen*/
-    public final static String TABLE_WANDS  = "all_wands";
+    public final static String TABLE_WANDS = "all_wands";
     public final static String COL_27 = "ID";
     public final static String COL_28 = "NAME";
     public final static String COL_29 = "LEVEL";
@@ -66,17 +67,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public final static String COL_33 = "AVG";
     /*-----------------------------------------------POTIONS-----------------------------------------------*/
     /* Tabelle für Potions erzeugen*/
-    public final static String TABLE_POTIONS  = "all_potions";
+    public final static String TABLE_POTIONS = "all_potions";
     public final static String COL_34 = "ID";
     public final static String COL_35 = "NAME";
     /*-----------------------------------------------Scroll-----------------------------------------------*/
     /* Tabelle für Scrolls erzeugen*/
-    public final static String TABLE_SCROLLS  = "all_scrolls";
+    public final static String TABLE_SCROLLS = "all_scrolls";
     public final static String COL_36 = "ID";
     public final static String COL_37 = "NAME";
     /*-----------------------------------------------Other Items-----------------------------------------------*/
     /* Tabelle für Other Items erzeugen*/
-    public final static String TABLE_OTHERITEMS  = "all_otheritems";
+    public final static String TABLE_OTHERITEMS = "all_otheritems";
     public final static String COL_38 = "ID";
     public final static String COL_39 = "NAME";
 
@@ -85,31 +86,108 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
+
     //Tabelename und die dazugehörigen Attribute erzeugen.
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_MELEE +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, BASE_STRENGTH INTEGER, ACCURACY INTEGER, DELAY REAL)");
-        db.execSQL("create table " + TABLE_MISSILE +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, REQUIRED_STRENGTH INTEGER, AVERAGE_HIT INTEGER, ACCURACY INTEGER, DELAY REAL)");
-        db.execSQL("create table " + TABLE_ENCHANTMENTS +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
-        db.execSQL("create table " + TABLE_ARMORS +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, LEVEL INTEGER, REQUIRED_STRENGTH INTEGER, ARMOR INTEGER, EFFECTIVE_ABSORPTION INTEGER)");
-        db.execSQL("create table " + TABLE_RINGS +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, LEVEL INTEGER, DURATION INTEGER, STEALTH INTEGER, DURATION_REDUCTION_FACTOR INTEGER, RESISTENCE_PROBABILITY INTEGER )");
-        db.execSQL("create table " + TABLE_WANDS +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, LEVEL INTEGER, DURATION INTEGER, MIN INTEGER, MAX INTEGER, AVG REAL)");
-        db.execSQL("create table " + TABLE_POTIONS +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
-        db.execSQL("create table " + TABLE_SCROLLS +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
-        db.execSQL("create table " + TABLE_OTHERITEMS +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
+        db.execSQL("create table " + TABLE_MELEE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, BASE_STRENGTH INTEGER, ACCURACY INTEGER, DELAY REAL)");
+        db.execSQL("create table " + TABLE_MISSILE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, REQUIRED_STRENGTH INTEGER, AVERAGE_HIT INTEGER, ACCURACY INTEGER, DELAY REAL)");
+        db.execSQL("create table " + TABLE_ENCHANTMENTS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
+        db.execSQL("create table " + TABLE_ARMORS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, LEVEL INTEGER, REQUIRED_STRENGTH INTEGER, ARMOR INTEGER, EFFECTIVE_ABSORPTION INTEGER)");
+        db.execSQL("create table " + TABLE_RINGS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, LEVEL INTEGER, DURATION INTEGER, STEALTH INTEGER, DURATION_REDUCTION_FACTOR INTEGER, RESISTENCE_PROBABILITY INTEGER )");
+        db.execSQL("create table " + TABLE_WANDS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT, LEVEL INTEGER, DURATION INTEGER, MIN INTEGER, MAX INTEGER, AVG REAL)");
+        db.execSQL("create table " + TABLE_POTIONS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
+        db.execSQL("create table " + TABLE_SCROLLS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
+        db.execSQL("create table " + TABLE_OTHERITEMS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TXT)");
     }
-
     //Dublikate löschen.
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_MELEE);
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_MISSILE);
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_ENCHANTMENTS);
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_ARMORS);
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_RINGS);
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_WANDS);
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_SCROLLS);
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_OTHERITEMS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MELEE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MISSILE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ENCHANTMENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARMORS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RINGS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_WANDS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SCROLLS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_OTHERITEMS);
         onCreate(db);
     }
+
+
+    public boolean insertFortMelee(String name, Integer base_strength, Integer accuracy, Double delay) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NAME", name);
+        contentValues.put("BASE_STRENGTH", base_strength);
+        contentValues.put("ACCURACY", accuracy);
+        contentValues.put("DELAY", delay);
+        long result = db.insert("weapons_melee", null, contentValues);
+        if (result == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     *
+     * @param DeleteDublicatForMelee
+     * @return dublicate in Zeilen löschen.
+     */
+    public Integer deleteDublicateForMelee(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+            return db.delete(TABLE_MELEE,"ID NOT IN (SELECT MIN(ID ) FROM weapons_melee GROUP BY NAME)", null);
+    }
+
+    /**
+     *
+     * @param DeleteDublicatForMissile
+     * @return dublicate in Zeilen löschen.
+     */
+    public Integer deleteDuplicateForMissile(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_MISSILE,"ID not in (SELECT MIN(ID ) FROM weapons_melee GROUP BY NAME)", null);
+    }
+
+    /**
+     *
+     * @param DeleteDublicatForRings
+     * @return dublicate in Zeilen löschen.
+     */
+    public Integer deleteDublicatForRings(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_RINGS,"ID not in (SELECT MIN(ID ) FROM weapons_melee GROUP BY NAME)", null);
+    }
+
+    /**
+     *
+     * @param DeleteDublicatForWands
+     * @return dublicate in Zeilen löschen.
+     */
+    public Integer deleteDublicatForWands(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_WANDS,"ID not in (SELECT MIN(ID ) FROM weapons_melee GROUP BY NAME)", null);
+    }
+
+    /**
+     *
+     * @param DeleteDublicatForScrolls
+     * @return dublicate in Zeilen löschen.
+     */
+    public Integer deleteDublicatForScrolls(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_SCROLLS,"ID not in (SELECT MIN(ID ) FROM weapons_melee GROUP BY NAME)", null);
+    }
+
+    /**
+     *
+     * @param DeleteDublicatForOtherItems
+     * @return dublicate in Zeilen löschen.
+     */
+    public Integer deleteDublicatForOtherItems(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_OTHERITEMS,"ID not in (SELECT MIN(ID ) FROM weapons_melee GROUP BY NAME)", null);
+    }
 }
+
--- app/src/main/java/com/example/vahel/PelDungeon/MainActivity.java
+++ app/src/main/java/com/example/vahel/PelDungeon/MainActivity.java
@@ -1,15 +1,50 @@
 package com.example.vahel.PelDungeon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelper myDb;
-
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDb = new DatabaseHelper(this);
+        db = new DatabaseHelper(this);
+
+        boolean insertFortMelee = db.insertFortMelee("XL", 2, 3, 2.2);
+        if(insertFortMelee==true) {
+            Toast.makeText(getApplicationContext(), "Successfully inserted for Melee", Toast.LENGTH_SHORT).show();
+        }
+
+        Integer deleteForMelee = db.deleteDublicateForMelee("");
+        if(deleteForMelee==null) {
+            Toast.makeText(getApplicationContext(), "Successfully deleted dublicate for Melee", Toast.LENGTH_SHORT).show();
+        }
+
+        Integer deleteForMissile = db.deleteDuplicateForMissile("");
+        if(deleteForMissile==null) {
+            Toast.makeText(getApplicationContext(), "Successfully deleted dublicate for Missile", Toast.LENGTH_SHORT).show();
+        }
+
+        Integer deleteForRings = db.deleteDublicatForRings("");
+        if(deleteForRings==null) {
+            Toast.makeText(getApplicationContext(), "Successfully deleted dublicate for Rings", Toast.LENGTH_SHORT).show();
+        }
+
+        Integer  deleteForWands = db.deleteDublicatForWands("");
+        if(deleteForWands==null) {
+            Toast.makeText(getApplicationContext(), "Successfully deleted dublicate for Wands", Toast.LENGTH_SHORT).show();
+        }
+
+        Integer  deleteForScrolls = db.deleteDublicatForScrolls("");
+        if(deleteForScrolls==null) {
+            Toast.makeText(getApplicationContext(), "Successfully deleted dublicate for Scrolls", Toast.LENGTH_SHORT).show();
+        }
+
+        Integer deleteForOtherItems = db.deleteDublicatForOtherItems("");
+        if(deleteForOtherItems==null) {
+            Toast.makeText(getApplicationContext(), "Successfully deleted dublicate for OtherItems", Toast.LENGTH_SHORT).show();
+        }
     }
 }
